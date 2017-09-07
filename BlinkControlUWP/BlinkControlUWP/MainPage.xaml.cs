c232f68dc11f6a895bf75bb737fbee1b6e75b80a
Tamilan S <tamils1809@outlook.com>
2017-09-07 22:28:38 +0530

    Sent message to Azure IoT Hub From UWP App

---
+++ BlinkControlUWP/BlinkControlUWP/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BlinkControlUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        //Install the Azure Service SDK Nuget; Add this--> Microsoft.Azure.Devices
+
+            //Copy and paste connectionstring from Azure Iot Hub Shared Policy--> Iothubbownwer
+        const string connectionString = "HostName=<YOURHUBNAME.azure-devices.net>;SharedAccessKeyName=iothubowner;SharedAccessKey=<YOUR HUB KEY>";
+        public MainPage()
+        {
+            this.InitializeComponent();
+            AddDevices();
+        }
+
+        void AddDevices()
+        {
+            //Type your Devices Name instead
+            RegistryManager manager = RegistryManager.CreateFromConnectionString(connectionString);
+            manager.AddDeviceAsync(new Device("YOUR DEVICE NAME"));
+        }
+
+        private async Task TurnLight(bool isOn)
+        {
+            await SendCloudToDeviceMessageAsync(isOn);
+        }
+
+        private static async Task SendCloudToDeviceMessageAsync(bool isOn)
+        {
+            var serviceClient = ServiceClient.CreateFromConnectionString(connectionString, TransportType.Amqp);
+            //var str = "Hello, Cloud, from a UWP app that uses Microsoft.Azure.Devices";
+            var message = new Message(System.Text.Encoding.ASCII.GetBytes(isOn ? "on" : "off"));
+            await serviceClient.SendAsync("YOUR DEVICE NAME", message);
+        }
+
+        private async void ToggleLedBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            await TurnLight(true);
+        }
+
+        private async void ToggleLedBtn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            await TurnLight(false);
+        }
+    }
+}
