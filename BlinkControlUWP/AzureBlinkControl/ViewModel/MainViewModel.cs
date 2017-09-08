133ffda1463287faf0270a99d45ad769a7b90edf
Tamilan S <tamils1809@outlook.com>
2017-09-08 16:12:47 +0530

    Added Raspberry Pi Project

---
+++ BlinkControlUWP/AzureBlinkControl/ViewModel/MainViewModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using GalaSoft.MvvmLight;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace AzureBlinkControl.ViewModel
+{
+    public class MainViewModel: ViewModelBase
+    {
+        #region GPIO Settings
+
+        public GpioController GpioController { get; }
+
+        public GpioPin LedPin { get; }
+
+        #endregion
+
+        #region Azure IoT Hub Settings
+
+        public DeviceClient DeviceClient { get; }
+
+        public string IotHubUri { get; } = ".azure-devices.net";
+
+        public string DeviceKey { get; } = "";
+
+        public string DeviceId => "";
+
+        #endregion
+
+        #region Display Fields
+
+        private bool _isAzureConnected;
+        private string _cloudToDeviceLog;
+
+        public bool IsAzureConnected
+        {
+            get { return _isAzureConnected; }
+            set { _isAzureConnected = value; RaisePropertyChanged(); }
+        }
+
+        public string CloudToDeviceLog
+        {
+            get { return _cloudToDeviceLog; }
+            set { _cloudToDeviceLog = value; RaisePropertyChanged(); }
+        }
+
+        #endregion
+
+        public MainViewModel()
+        {
+            DeviceClient = DeviceClient.Create(IotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(DeviceId, DeviceKey));
+
+            GpioController = GpioController.GetDefault();
+            if (null != GpioController)
+            {
+                LedPin = GpioController.OpenPin(4);
+                LedPin.SetDriveMode(GpioPinDriveMode.Output);
+            }
+        }
+
+        public async Task SendDeviceToCloudMessagesAsync()
+        {
+            try
+            {
+                var telemetryDataPoint = new
+                {
+                    deviceId = DeviceId,
+                    message = "Hello"
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await DeviceClient.SendEventAsync(message);
+                Debug.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                IsAzureConnected = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task ReceiveCloudToDeviceMessageAsync()
+        {
+            CloudToDeviceLog = "Receiving messages...";
+            Debug.WriteLine("\nReceiving cloud to device messages from service");
+
+            while (true)
+            {
+                Message receivedMessage = await DeviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                var msg = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                CloudToDeviceLog += "\nReceived message: " + msg;
+
+                if (msg == "on")
+                {
+                    LedPin.Write(GpioPinValue.Low);
+                }
+
+                if (msg == "off")
+                {
+                    LedPin.Write(GpioPinValue.High);
+                }
+
+                await DeviceClient.CompleteAsync(receivedMessage);
+            }
+        }
+    }
+}
---
+++ BlinkControlUWP/AzureBlinkControl/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace AzureBlinkControl.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
